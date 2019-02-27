00fbfb6d6a8cfbeb78b8077ab435dbdc839caf77
root <carlosvilela.rj@gmail.com>
2019-02-27 14:52:32 -0300

    Projeto Logico - criando Button

--- app/src/main/java/system/volumenotification/MainActivity.java
+++ app/src/main/java/system/volumenotification/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 .setOngoing(true)
                 .build();
 
-        vNotification.bigContentView = visualNotification;
+        vNotification.contentView = visualNotification;
 
 
 
